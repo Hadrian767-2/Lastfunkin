9acaff9006a2a72fa09a91a14705094a203e8f62
Hadrian767-2 <hadriannfs976@gmail.com>
2025-01-09 16:37:23 -0300

    Initial commit

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -288,7 +288,7 @@ class PlayState extends MusicBeatState
 		FlxG.cameras.add(camOther);
 		grpNoteSplashes = new FlxTypedGroup<NoteSplash>();
 
-		dynamic FlxG.cameras.setDefaultDrawTarget = [camGame]}; 
+		public static var FlxG.cameras.setDefaultDrawTarget = [camGame]; 
 		CustomFadeTransition.nextCamera = camOther;
 		//FlxG.cameras.setDefaultDrawTarget(camGame, true);
 
