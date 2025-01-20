a839632390e0089b19b317bea4c1c6c3594f2e93
Hadrian767-2 <hadriannfs976@gmail.com>
2025-01-19 22:38:15 -0300

    Initial commit

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -50,7 +50,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.cameras.reset(camGame);
 		FlxG.cameras.add(camAchievement);
-		dynamic FlxG.cameras.setDefaultDrawTarget = camGame;
+		FlxG.cameras.setDefaultDrawTarget(camGame);
 
 		transIn = FlxTransitionableState.defaultTransIn;
 		transOut = FlxTransitionableState.defaultTransOut;
